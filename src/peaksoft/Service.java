73de725517d6bcea655c70052fd3fabbfad3ea1f
adilet524 <zhumakadyrovadilet524@gmail.com>
2022-07-09 22:45:45 +0600

    Howe work autobase!

---
+++ src/peaksoft/Service.java
@@ -0,0 +1,9 @@
+package peaksoft;
+
+public interface Service {
+    void changeDriver(Truck truck, Driver driver);
+
+    void startDriving(Truck truck, Driver driver);
+
+    void startRepair(Truck truck, Driver driver);
+}
